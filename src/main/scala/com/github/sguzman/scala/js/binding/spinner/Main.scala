2df0ea6a8b061a11d8edb2f3d82b7a784a422a65
Salvador Guzman <guzmansalv@gmail.com>
2018-01-04 19:26:32 -0800

    spinner

--- src/main/scala/com/github/sguzman/scala/js/binding/spinner/Main.scala
+++ src/main/scala/com/github/sguzman/scala/js/binding/spinner/Main.scala
@@ -1,7 +1,20 @@
 package com.github.sguzman.scala.js.binding.spinner
 
+import com.thoughtworks.binding.Binding.Var
+import com.thoughtworks.binding.{Binding, dom}
+import org.scalajs.dom.html.Div
+import org.scalajs.dom.{Event, document}
+
 object Main {
+  @dom def spinner(count: Var[Int]): Binding[Div] = {
+    <div>
+      <button onclick={e: Event => println("Clicked on decr button")}>-</button>
+      {count.bind.toString}
+      <button onclick={e: Event => println("Clicked on incr button")}>+</button>
+    </div>
+  }
+
   def main(args: Array[String]): Unit = {
-    println("Hello world")
+    dom.render(document.body, spinner(Var(0)))
   }
 }
