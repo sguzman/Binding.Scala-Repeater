663e60b6debf696df328cc0779c192a7d972069d
Salvador Guzman <guzmansalv@gmail.com>
2018-01-04 19:38:07 -0800

    simple spinner

--- src/main/scala/com/github/sguzman/scala/js/binding/spinner/Main.scala
+++ src/main/scala/com/github/sguzman/scala/js/binding/spinner/Main.scala
@@ -2,19 +2,27 @@ package com.github.sguzman.scala.js.binding.spinner
 
 import com.thoughtworks.binding.Binding.Var
 import com.thoughtworks.binding.{Binding, dom}
+import org.scalajs.dom.document
 import org.scalajs.dom.html.Div
-import org.scalajs.dom.{Event, document}
+import org.scalajs.dom.raw.MouseEvent
 
 object Main {
   @dom def spinner(count: Var[Int]): Binding[Div] = {
     <div>
-      <button onclick={e: Event => println("Clicked on decr button")}>-</button>
+      <button onclick={_: MouseEvent => count.value_=(count.value - 1)}>-</button>
       {count.bind.toString}
-      <button onclick={e: Event => println("Clicked on incr button")}>+</button>
+      <button onclick={_: MouseEvent => count.value_=(count.value + 1)}>+</button>
+    </div>
+  }
+
+  @dom def render(init: Var[Int]): Binding[Div] = {
+    <div>
+      {spinner(init).bind}
+      <h1>At count {init.bind.toString}</h1>
     </div>
   }
 
   def main(args: Array[String]): Unit = {
-    dom.render(document.body, spinner(Var(0)))
+    dom.render(document.body, render(Var(0)))
   }
 }
